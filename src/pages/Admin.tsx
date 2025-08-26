780805b519b8b224f92b3956b2c383eb5d5ea1f7
tjrmswo <93309061+tjrmswo@users.noreply.github.com>
2025-08-26 15:06:41 +0900

    refactor: admin 페이지 구조 리펙토링

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,52 +1,25 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
-import {
-  AdminSideBar,
-  BulletinManager,
-  DashboardDefault,
-  NewsManager,
-  NoticeManager,
-  WorshipManager,
-} from "@/features/admin";
+import { AdminSideBar } from "@/features/admin";
 import { fadeInUp } from "@/shared/constants/FadeInUp";
+import { useCommonStore } from "@/features/admin/store/common";
+import useAdminHandler from "@/features/admin/utils/Admin";
 
 const Admin = () => {
-  const [activeTab, setActiveTab] = useState("dashboard");
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
-  const renderContent = () => {
-    switch (activeTab) {
-      case "dashboard":
-        return <DashboardDefault setActiveTab={setActiveTab} />;
-      case "bulletins":
-        return <BulletinManager />; // 주보 관리
-      case "worship":
-        return <WorshipManager />; // 예배 일정
-      case "news":
-        return <NewsManager />; //교회 소식
-      case "notice":
-        return <NoticeManager />; // 공지사항
-      default:
-        return <div>페이지를 찾을 수 없습니다</div>;
-    }
-  };
-
-  const handleMenu = () => setIsOpen(!isOpen);
+  const tab = useCommonStore((state) => state.activeTab);
+  const { handleMenu, renderContent } = useAdminHandler({ setIsOpen });
 
   return (
     <div className="min-h-screen w-full bg-gray-50">
       <div className="flex">
-        <AdminSideBar
-          setActiveTab={setActiveTab}
-          activeTab={activeTab}
-          handleMenu={handleMenu}
-          isOpen={isOpen}
-        />
+        <AdminSideBar handleMenu={handleMenu} isOpen={isOpen} />
 
         {/* 메인 컨텐츠 */}
         <div className="flex-1 p-8">
           <motion.div
-            key={activeTab}
+            key={tab}
             initial="initial"
             animate="animate"
             variants={fadeInUp}
