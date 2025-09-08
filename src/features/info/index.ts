9b148295754e732dc5d9135420daab5f0339dd8f
tjrmswo <93309061+tjrmswo@users.noreply.github.com>
2025-09-08 18:26:42 +0900

    refactor: 갤러리 페이지 도메인 재정립

--- src/features/info/index.ts
+++ src/features/info/index.ts
@@ -1,1 +1,4 @@
 export { default as WorshipContact } from "./ui/WorshipContact";
+export { default as ChurchContact } from "./ui/ChurchContact";
+export { default as TransportGuide } from "./ui/TransportGuide";
+export { default as ParkingGuide } from "./ui/ParkingGuide";
